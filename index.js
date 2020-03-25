2756eb5755cf9d9ada7211d14d07f3c708deb992
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-25 06:12:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 document.addEventListener("DOMContentLoaded", function(){
   console.log("The DOM has loaded");
   let text = document.getElementById("text")
-  text.innerHTML = 
+  text.innerHTML = "This is really cool!";
 });
 console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
